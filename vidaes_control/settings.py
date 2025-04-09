f0d30b8da1f04c4c9d005f7f7bce03e6d63204f9
Sebastian <sebastian.smp21@gmail.com>
2025-04-08 23:05:51 -0400

    add csrf

--- vidaes_control/settings.py
+++ vidaes_control/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://vidaes-production.up.railway.app',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
